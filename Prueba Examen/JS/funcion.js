559d88e1953fc492b9ee3a950f2ac0fd0ab2a193
Berkan Raimov <berkanraimov@gmail.com>
2018-01-23 20:32:31 +0100

    Examen

---
+++ Prueba Examen/JS/funcion.js
@@ -0,0 +1,10 @@
+window.onload();
+
+function openandCloseDiv() {
+    if (document.getElementById("toogle").style.display == "none") {
+        document.getElementById("toogle").style.display = "block"
+        }
+        else{
+            document.getElementById("toogle").style.display = "none";
+        }          
+}
